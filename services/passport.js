3f0eb31f5c9ed7fba2e92968e75581064991c4d4
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-08-11 12:22:12 -0700

    add mongoose and User model

---
+++ services/passport.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with this profile ID
+        } else {
+          new User({
+            googleId: profile.id
+          }).save();
+        }
+      });
+    }
+  )
+);
