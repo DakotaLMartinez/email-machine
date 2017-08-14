c3fd2b846d0aeebebfbc4de9aed7fdd1546db5d4
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-08-14 12:06:06 -0700

    add authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,8 @@
+// State argument is not application state, only the state
+// this reducer is responsible for
+export default function(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
