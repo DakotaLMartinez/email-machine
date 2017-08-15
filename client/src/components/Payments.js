8b4296259c2e5235aaf51e05bf87848c7ada6146
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-08-15 13:56:21 -0700

    change button and add description to popup

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -13,10 +13,14 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="EmailMachine"
+        description="$5 for 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
